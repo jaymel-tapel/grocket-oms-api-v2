46d60487789af5ab584072a3da44b8983c3ad58d
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-13 19:28:42 +0800

    Added DTO for User; Implemented Swagger

---
+++ src/modules/users/dto/update-user.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
+import { Prisma } from '@prisma/client';
+
+export class UpdateUserDto
+  extends PartialType(CreateUserDto)
+  implements Prisma.UserUpdateInput {}
