35f27c817eb0e48f6740bc4b7101352c7a680588
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-13 17:44:04 +0800

    Added DB Module; Implemented Soft Delete

---
+++ src/modules/database/services/database.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { createSoftDeleteExtension } from 'prisma-extension-soft-delete';
+
+@Injectable()
+export class DatabaseService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async softDelete() {
+    return this.$extends(
+      createSoftDeleteExtension({
+        models: {
+          User: true,
+        },
+        defaultConfig: {
+          field: 'deletedAt',
+          createValue: (deleted) => {
+            if (deleted) return new Date();
+            return null;
+          },
+        },
+      }),
+    );
+  }
+
+  async onModuleInit() {
+    return await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    return await this.$disconnect();
+  }
+}
