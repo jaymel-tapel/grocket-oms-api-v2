f22b894ee58bee75581bd23f3d822b65f4e44bd1
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-13 19:50:46 +0800

    Added Entity for User

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { $Enums, User } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+
+export class UserEntity implements User {
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  @Exclude()
+  password: string;
+
+  @ApiProperty({ enum: $Enums.RoleEnum })
+  role: $Enums.RoleEnum;
+
+  @ApiPropertyOptional({ nullable: true, default: null })
+  contact_url: string | null;
+
+  @ApiPropertyOptional({ nullable: true, default: null })
+  phone: string | null;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiPropertyOptional({ nullable: true, default: null })
+  deletedAt: Date | null;
+}
