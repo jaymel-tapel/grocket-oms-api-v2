f01b003c35def2fd200278976f4564a79010ba7f
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-13 19:38:36 +0800

    Moved All Exception Filter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AllExceptionsFilter } from './all-exception-filter/all-exception-filter.filter';
+import { AllExceptionsFilter } from './common/filters/all-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
