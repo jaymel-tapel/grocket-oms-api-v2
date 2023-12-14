464093834f85412eefcb3ce09a00b0f7e958819d
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-14 17:08:26 +0800

    Improved Error Handling

--- src/common/filters/all-exception.filter.ts
+++ src/common/filters/all-exception.filter.ts
@@ -14,23 +14,39 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    let message = exception.message?.replace(/\n/g, '') ?? 'Internal Server Error';
+    let message = 'Internal Server Error';
     let stack = exception.stack?.replace(/\n/g, '') ?? null;
     let statusCode: number = HttpStatus.INTERNAL_SERVER_ERROR;
 
-    if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+    if (exception instanceof HttpException) {
+      statusCode = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
+
+      if (typeof exceptionResponse === 'string') {
+        message = exceptionResponse;
+      } else if (
+        typeof exceptionResponse === 'object' &&
+        'message' in exceptionResponse
+      ) {
+        message = (exceptionResponse as { message: string }).message;
+      }
+    } else if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error(exception.code);
       switch (exception.code) {
         case 'P2002': {
           statusCode = HttpStatus.CONFLICT;
+          message = `Unique constraint failed on the fields: ${exception.meta.target}`;
+          break;
+        }
+        case 'P2003': {
+          statusCode = HttpStatus.BAD_REQUEST;
+          message = exception.message;
           break;
         }
         default:
-          super.catch(exception, host);
+          console.error(exception);
           break;
       }
-    }
-    else if (exception instanceof HttpException) {
-      statusCode = exception.getStatus();
     } else {
       // Handle other types of exceptions
       message = exception.message || message; // Use the exception message if available
@@ -39,7 +55,7 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
 
     response.status(statusCode).json({
       statusCode,
-      message,
+      message: message.replace(/\n/g, ''),
       stack,
       timestamp: new Date().toISOString(),
     });
