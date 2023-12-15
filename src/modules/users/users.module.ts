c7c9954b3b1dfe194d326171a7f271ec2ff4b1db
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-15 23:18:38 +0800

    Added Hash Service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { UsersService } from './services/users.service';
 import { UsersController } from './users.controller';
+import { HashService } from '../auth/services/hash.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService,]
+  providers: [UsersService, HashService],
+  exports: [UsersService],
 })
 export class UsersModule {}
