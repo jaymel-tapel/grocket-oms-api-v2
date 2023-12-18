8dea8847fd96f893a035830e658ff84baffa63b2
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-18 20:31:24 +0800

    Remove Alternate Email Controller

--- src/modules/alternate-emails/alternate-emails.module.ts
+++ src/modules/alternate-emails/alternate-emails.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AlternateEmailsService } from './services/alternate-emails.service';
-import { AlternateEmailsController } from './alternate-emails.controller';
 import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AlternateEmailsController],
   providers: [AlternateEmailsService],
   exports: [AlternateEmailsService],
 })
