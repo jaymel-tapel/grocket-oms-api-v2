408372884703c8d2f6dc810d128807fa51b44738
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-18 20:32:10 +0800

    Created upsert for alternate email

--- src/modules/alternate-emails/services/alternate-emails.service.ts
+++ src/modules/alternate-emails/services/alternate-emails.service.ts
@@ -1,12 +1,11 @@
-import {
-  ConflictException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/modules/database/services/database.service';
 import { Prisma } from '@prisma/client';
 import { UsersService } from 'src/modules/users/services/users.service';
 import { FindAlternateDto } from '../dto/find-alternate-email.dto';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { AlternateEmailDto } from '../dto/alternate-email.dto';
+import { AlternateEmailEntity } from '../entities/alternate-email.entity';
 
 @Injectable()
 export class AlternateEmailsService {
@@ -15,20 +14,48 @@ export class AlternateEmailsService {
     private readonly userService: UsersService,
   ) {}
 
-  async create(
-    createAlternateEmailDto: Prisma.AlternateEmailUncheckedCreateInput,
-  ) {
+  async upsert(user: UserEntity, alterEmailDto: AlternateEmailDto[]) {
+    const emails: AlternateEmailEntity[] = [];
+
     const result = await this.database.$transaction(async (tx) => {
-      const foundEmail = await this.userService.findByCondition({
-        where: { email: createAlternateEmailDto.email },
+      // ? Force Delete All alternate emails that connects to the user
+      await tx.alternateEmail.deleteMany({
+        where: {
+          userId: user.id,
+        },
       });
 
-      // ? Check if userId does exist
-      await this.userService.findOne(createAlternateEmailDto.userId);
+      if (alterEmailDto.length === 0) {
+        return [];
+      }
 
-      if (foundEmail) throw new ConflictException('Email already exists');
+      for (const dto of alterEmailDto) {
+        const checkUser = await this.userService.findAllByCondition({
+          where: { email: dto.email },
+        });
 
-      return await tx.alternateEmail.create({ data: createAlternateEmailDto });
+        const foundAlternateEmail = await this.findByCondition({
+          where: {
+            userId: {
+              not: user.id,
+            },
+            email: dto.email,
+          },
+        });
+
+        if (!foundAlternateEmail && checkUser.length === 0) {
+          // ? If alternate email doesn't exist yet, then Create new Alternate Email
+          const newAlternateEmail = await tx.alternateEmail.create({
+            data: {
+              userId: user.id,
+              email: dto.email,
+            },
+          });
+          emails.push(newAlternateEmail);
+        }
+      }
+
+      return emails;
     });
 
     return result;
@@ -47,6 +74,11 @@ export class AlternateEmailsService {
     });
   }
 
+  async findAllByCondition(args: Prisma.AlternateEmailFindManyArgs) {
+    const database = await this.database.softDelete();
+    return await database.alternateEmail.findMany(args);
+  }
+
   async findOne(id: number) {
     const database = await this.database.softDelete();
     const user = await database.alternateEmail.findUniqueOrThrow({
@@ -59,23 +91,17 @@ export class AlternateEmailsService {
     return user;
   }
 
-  async update(
-    id: number,
-    updateAlternateEmailDto: Prisma.AlternateEmailUpdateInput,
-  ) {
+  async findByCondition(args: Prisma.AlternateEmailFindFirstArgs) {
     const database = await this.database.softDelete();
-    const result = await database.$transaction(async (tx) => {
-      return await tx.alternateEmail.update({
-        where: { id },
-        data: updateAlternateEmailDto,
-      });
-    });
-
-    return result;
+    return await database.alternateEmail.findFirst(args);
   }
 
   async remove(id: number) {
     const database = await this.database.softDelete();
     return await database.alternateEmail.delete({ where: { id } });
+  }
+
+  async forceRemove(id: number) {
+    return await this.database.alternateEmail.delete({ where: { id } });
   }
 }
