dad3079ea4965f3fbc1927c855a0d115f4895069
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-01-08 23:41:31 +0800

    Added Validation if Filter is Defined then there should be also a keyword

--- src/modules/users/dto/filter-user.dto.ts
+++ src/modules/users/dto/filter-user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { FilterDto } from '@src/common/dtos/search-filter.dto';
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, ValidateIf } from 'class-validator';
 
 export enum FilterUserEnum {
   ID = 'id',
@@ -12,4 +12,8 @@ export class FilterUsersDto extends FilterDto {
   @IsEnum(FilterUserEnum)
   @ApiPropertyOptional()
   filter?: FilterUserEnum;
+
+  @IsNotEmpty()
+  @ValidateIf((o) => o.filter !== undefined)
+  keyword?: string;
 }
