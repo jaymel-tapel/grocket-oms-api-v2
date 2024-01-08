4fa4bb6ac9516de5a9455000e9889b5d4f058ddd
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-01-08 21:37:55 +0800

    Separated the 'filter' property

--- src/modules/users/dto/filter-user.dto.ts
+++ src/modules/users/dto/filter-user.dto.ts
@@ -1,6 +1,7 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { FilterDto } from '@src/common/dtos/search-filter.dto';
 import { IsEnum, IsOptional } from 'class-validator';
+
 export enum FilterUserEnum {
   ID = 'id',
   EMAIL = 'email',
