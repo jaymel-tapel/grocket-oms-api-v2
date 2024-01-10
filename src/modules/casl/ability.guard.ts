2b735adc0bc142bae473385c6a87b1e8a4959fb0
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-01-10 17:34:49 +0800

    Added Ability Module using CASL

---
+++ src/modules/casl/ability.guard.ts
@@ -0,0 +1,42 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ForbiddenError } from '@casl/ability';
+import { AbilityFactory } from './ability.factory';
+import { CHECK_ABILITY, RequiredRule } from './ability.decorator';
+
+@Injectable()
+export class CaslGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private abilityFactory: AbilityFactory,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const rules =
+      this.reflector.get<RequiredRule[]>(CHECK_ABILITY, context.getHandler()) ||
+      [];
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user; // Get the authenticated user
+
+    // console.log(request.user);
+    const ability = await this.abilityFactory.defineAbility(user);
+
+    try {
+      rules.forEach((rule) =>
+        ForbiddenError.from(ability).throwUnlessCan(rule.action, rule.subject),
+      );
+
+      return true;
+    } catch (error) {
+      if (error instanceof ForbiddenError) {
+        throw new ForbiddenException(error.message);
+      }
+    }
+  }
+}
