1add9d84f588104c7a516a7d5396aecef50da78e
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-01-10 17:38:55 +0800

    Made it Global

--- src/modules/casl/ability.module.ts
+++ src/modules/casl/ability.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AbilityFactory } from './ability.factory';
 
+@Global()
 @Module({
   providers: [AbilityFactory],
   exports: [AbilityFactory],
