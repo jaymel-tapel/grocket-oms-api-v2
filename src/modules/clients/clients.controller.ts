e0c863e292074a6020d5af40cc59b0915d65eb0c
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-01-10 20:40:27 +0800

    Added RBAC

--- src/modules/clients/clients.controller.ts
+++ src/modules/clients/clients.controller.ts
@@ -8,24 +8,41 @@ import {
   Delete,
   ParseIntPipe,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ClientsService } from './services/clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiExtraModels,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UsersService } from '../users/services/users.service';
 import { ClientEntity } from './entities/client.entity';
 import { TransferClientsDto } from './dto/transfer-client.dto';
 import { ApiPageResponse } from '@modules/page/api-page-response.decorator';
 import { ConnectionArgsDto } from '@modules/page/connection-args.dto';
 import { FilterClientsDto } from './dto/filter-client.dto';
+import { JwtGuard } from '@modules/auth/guard';
+import { PageEntity } from '@modules/page/page.entity';
+import { AbilityFactory, Action } from '@modules/casl/ability.factory';
+import { AuthUser } from '@modules/auth/decorator/auth-user.decorator';
+import { UserEntity } from '@modules/users/entities/user.entity';
+import { ForbiddenError } from '@casl/ability';
 
+@UseGuards(JwtGuard)
 @ApiTags('clients')
 @Controller('clients')
+@ApiBearerAuth()
+@ApiExtraModels(PageEntity)
 export class ClientsController {
   constructor(
     private readonly clientsService: ClientsService,
     private readonly usersService: UsersService,
+    private readonly abilityFactory: AbilityFactory,
   ) {}
 
   @Post()
@@ -44,12 +61,10 @@ export class ClientsController {
   @Get()
   @ApiPageResponse(ClientEntity)
   async findAll(
+    @AuthUser() user: UserEntity,
     @Query() findManyArgs: FilterClientsDto,
     @Query() connectionArgs: ConnectionArgsDto,
   ) {
-    // ! Temporary User for testing purposes
-    const user = await this.usersService.findUniqueOrThrow(3);
-
     return await this.clientsService.findAllPagination(
       user,
       findManyArgs,
@@ -59,13 +74,22 @@ export class ClientsController {
 
   @Get(':id')
   @ApiOkResponse({ type: ClientEntity })
-  async findOne(@Param('id', ParseIntPipe) id: number) {
-    return new ClientEntity(
-      await this.clientsService.findOne(
-        { id },
-        { include: { clientInfo: true, seller: true } },
-      ),
+  async findOne(
+    @AuthUser() authUser: UserEntity,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    const ability = await this.abilityFactory.defineAbility(authUser);
+    const client = await this.clientsService.findOne(
+      { id },
+      { include: { clientInfo: true, seller: true } },
     );
+
+    ForbiddenError.from(ability).throwUnlessCan(
+      Action.Read,
+      new ClientEntity(client),
+    );
+
+    return new ClientEntity(client);
   }
 
   @Patch(':id')
