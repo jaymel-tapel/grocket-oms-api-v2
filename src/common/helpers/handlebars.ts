3503993907e94758855e3c36b4db71f7246edffe
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-01-31 23:12:15 +0800

    Created a Custom HandlebarsAdapter

---
+++ src/common/helpers/handlebars.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import * as Handlebars from 'handlebars';
+
+const handlebarsHelpers = {
+  eq: (v1, v2) => v1 === v2,
+  neq: (v1, v2) => v1 !== v2,
+  lt: (v1, v2) => v1 < v2,
+  gt: (v1, v2) => v1 > v2,
+  lte: (v1, v2) => v1 <= v2,
+  gte: (v1, v2) => v1 >= v2,
+  and() {
+    return Array.prototype.every.call(arguments, Boolean);
+  },
+  or() {
+    return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+  },
+};
+
+@Injectable()
+export class CustomHandlebarsAdapter extends HandlebarsAdapter {
+  constructor() {
+    super();
+    this.registerCustomHelpers();
+  }
+
+  private registerCustomHelpers() {
+    Handlebars.registerHelper(handlebarsHelpers);
+  }
+}
