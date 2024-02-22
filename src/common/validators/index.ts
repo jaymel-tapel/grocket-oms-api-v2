7c4552e7185f5798c72f499d6bb868f638ecb52c
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-02-22 16:15:43 +0800

    Added Prisma Tables

--- src/common/validators/index.ts
+++ src/common/validators/index.ts
@@ -1,7 +1,9 @@
 import { IsAlreadyExistConstraint } from './isAlreadyExist.validation';
+import { IsValidPrismaTableConstraint } from './prismaTables.validation';
 import { DoesExistConstraint } from './user.validation';
 
 export const ValidatorConstraints = [
   DoesExistConstraint,
   IsAlreadyExistConstraint,
+  IsValidPrismaTableConstraint,
 ];
---
+++ src/common/validators/prismaTables.validation.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import {
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  registerDecorator,
+} from 'class-validator';
+import { TableNameTypes } from '../csv/dto/create-csv.dto';
+import { Prisma } from '@prisma/client';
+import _ from 'lodash';
+
+const modelNames = Object.values(Prisma.ModelName);
+export const FormattedModelNames = modelNames.map((modelName) =>
+  _.lowerFirst(modelName),
+);
+
+@Injectable()
+@ValidatorConstraint({ async: false })
+export class IsValidPrismaTableConstraint
+  implements ValidatorConstraintInterface
+{
+  validate(value: TableNameTypes, args?: ValidationArguments) {
+    return FormattedModelNames.includes(value);
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `Invalid table value. Expected values are: ${FormattedModelNames.join(
+      ', ',
+    )}`;
+  }
+}
+
+// decorator function
+export function IsValidPrismaTable(validationOptions?: ValidationOptions) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsValidPrismaTableConstraint,
+    });
+  };
+}
