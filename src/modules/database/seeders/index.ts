4682bd09a488b4e78f8209800b4e6d9338ab7282
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-02-22 16:16:30 +0800

    Added Command for Updating Client Passwords

--- src/modules/database/seeders/index.ts
+++ src/modules/database/seeders/index.ts
@@ -1,4 +1,11 @@
+import { Logger } from '@nestjs/common';
+import { ClientPasswordCommand } from '../commands/client-password';
 import { ClientIndustryCommand } from './client-industry-seed';
 import { ClientSourceCommand } from './client-source-seed';
 
-export const Commands = [ClientSourceCommand, ClientIndustryCommand];
+export const Commands = [
+  ClientSourceCommand,
+  ClientIndustryCommand,
+  ClientPasswordCommand,
+  Logger,
+];
