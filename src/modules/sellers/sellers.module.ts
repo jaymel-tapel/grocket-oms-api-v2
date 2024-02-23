a2c7c93116717e17774c98ef308d7a3d1d54351f
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-02-23 16:18:50 +0800

    Added AlternateEmail Module

--- src/modules/sellers/sellers.module.ts
+++ src/modules/sellers/sellers.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SellersController } from './sellers.controller';
 import { SellersReportService } from './services/sellers-report.service';
 import { SellersService } from './services/sellers.service';
+import { AlternateEmailsModule } from '@modules/alternate-emails/alternate-emails.module';
 
 @Module({
+  imports: [AlternateEmailsModule],
   controllers: [SellersController],
   providers: [SellersReportService, SellersService],
 })
