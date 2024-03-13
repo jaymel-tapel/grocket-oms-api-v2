6c938bef69748966f1e1fb1efc637d57ae3a0874
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-03-13 22:39:28 +0800

    Added Prospect Template Seeder

--- src/modules/database/seeders/index.ts
+++ src/modules/database/seeders/index.ts
@@ -2,10 +2,12 @@ import { Logger } from '@nestjs/common';
 import { ClientPasswordCommand } from '../commands/client-password';
 import { ClientIndustryCommand } from './client-industry-seed';
 import { ClientSourceCommand } from './client-source-seed';
+import { ProspectTemplateCommand } from './prospect-template-seed';
 
 export const Commands = [
   ClientSourceCommand,
   ClientIndustryCommand,
   ClientPasswordCommand,
+  ProspectTemplateCommand,
   Logger,
 ];
