0faddbef20f65f5368b3ac7100832115db41aa36
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-03-21 00:14:01 +0800

    Added Dynamic Reviews and Ratings for send email

--- src/modules/prospects/services/prospect-send-email.service.ts
+++ src/modules/prospects/services/prospect-send-email.service.ts
@@ -23,7 +23,37 @@ export class ProspectSendMailService {
     let ctr = 0;
 
     if (emails?.length === 0) {
-      throw new HttpException(`No recipients defined`, 400);
+      throw new HttpException(`No recipient(s) found`, 404);
+    }
+
+    const total_reviews = prospect.reviews;
+    const countStars = prospect.stars;
+    const rating = prospect.rating;
+
+    const percentages = countStars.map((star, i) => {
+      const percent = +((star / total_reviews) * 100).toFixed(2);
+      return { [5 - i]: percent };
+    });
+
+    const starClass = [null, null, null, null, null];
+
+    // ? Get the whole number of rating. ex: 4.5 -> 4
+    const wholeRating = Math.floor(rating);
+
+    // ? Get the decimal value of rating. ex: 4.5 -> 0.5
+    const roundUp = +(rating % 1).toFixed(2);
+
+    // ? Determine if there's a half star
+    if (roundUp >= 0.4 && roundUp < 0.9) {
+      starClass[wholeRating] = 'half-star';
+    } else if (roundUp < 0.4 && rating < 5) {
+      // ? If no half star, check for empty star
+      starClass[wholeRating] = 'empty-star';
+    }
+
+    // ? Fill remaining slots with empty stars
+    for (let i = wholeRating + 1; i < 5; i++) {
+      starClass[i] = 'empty-star';
     }
 
     for (const recipient of emails) {
@@ -32,12 +62,18 @@ export class ProspectSendMailService {
           to: recipient,
           subject: template.subject,
           template: 'prospect-email-template',
-          context: { dynamicContent },
+          context: {
+            rating,
+            total_reviews,
+            starClass,
+            percentages,
+            dynamicContent,
+          },
         });
 
         await this.prospectLogsService.createLog(prospect.id, authUser, {
           templateId: template.id,
-          action: `${template.name} template`,
+          action: `Email sent successfully`,
         });
       } catch (error) {
         ctr++;
