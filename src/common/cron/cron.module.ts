48554e1f81e49906ec151db4a6338300f7d30a28
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-03-25 22:19:44 +0800

    Added Task Module

--- src/common/cron/cron.module.ts
+++ src/common/cron/cron.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OrdersCronService } from './services/orders-cron.service';
+import { TasksModule } from '@modules/my-tasks/tasks.module';
 
 @Module({
+  imports: [TasksModule],
   providers: [OrdersCronService],
   exports: [OrdersCronService],
 })
