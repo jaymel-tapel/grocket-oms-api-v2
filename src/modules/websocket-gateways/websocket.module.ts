ea89fde3dc7e9157f768173a610c9aa37e6a804c
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-04-18 22:35:01 +0800

    Moved to REST API

--- src/modules/websocket-gateways/websocket.module.ts
+++ src/modules/websocket-gateways/websocket.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ChatsGateway } from './chats.gateway';
-import { HashService } from '@modules/auth/services/hash.service';
-import { JwtService } from '@nestjs/jwt';
-import { ParticipantsModule } from '@modules/participants/participants.module';
-import { ConversationsModule } from '@modules/conversations/conversations.module';
-import { MessagesModule } from '@modules/messages/messages.module';
-import { ChatsService } from '../chats/services/chats.service';
-import { ChatsModule } from '@modules/chats/chats.module';
 
 @Module({
-  imports: [
-    ChatsModule,
-    ParticipantsModule,
-    ConversationsModule,
-    MessagesModule,
-  ],
-  providers: [ChatsGateway, HashService, JwtService],
+  providers: [ChatsGateway],
   exports: [ChatsGateway],
 })
 export class WebsocketModule {}
